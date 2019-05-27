51d8aa28b93f0fd70f2474928c01dd5e2f9e16d4
khushbuS <khusic4@gmail.com>
2019-05-26 19:24:39 -0700

    added recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Pasta', 'delicious pasta', 'https://www.seriouseats.com/recipes/images/2016/08/20160827-cherry-tomato-pasta-13.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
